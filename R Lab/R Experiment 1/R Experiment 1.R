96ebb7f4dbfee04305c4f55c2ca37ccf1d5aceec
Dyotak <dyotak10@gmail.com>
2024-01-21 20:53:15 +0530

    Finished

--- R Lab/R Experiment 1/R Experiment 1.R
+++ R Lab/R Experiment 1/R Experiment 1.R
@@ -1,3 +1,9 @@
+# Experiment 1a
+
+name = readline(prompt="Input your name: ")
+age =  readline(prompt="Input your age: ")
+print(paste("My name is",name, "and I am",age ,"years old."))
+print(R.version.string)
 
 
 

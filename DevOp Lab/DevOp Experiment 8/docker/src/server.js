26cc0f1ffb7e5e2376c892dbc5f43c704019005f
Dyotak <dyotak10@gmail.com>
2024-03-25 19:15:15 +0530

    Kubernetes

---
+++ DevOp Lab/DevOp Experiment 8/docker/src/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome! to home page.");
+});
+
+app.listen(3000, () => {
+    console.log("app listening on port 3000.")
+});
